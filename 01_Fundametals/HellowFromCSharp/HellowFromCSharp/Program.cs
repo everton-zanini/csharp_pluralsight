b7af60df552ef077b6eb346150caa1a2f47e6092
Everton Zanini <107217906+everton-zanini@users.noreply.github.com>
2023-01-03 16:30:02 -0300

    Gettings Started completed

---
+++ 01_Fundametals/HellowFromCSharp/HellowFromCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, everybody");
+
+Console.WriteLine("Please enter yopur name: ");
+
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name);
